9beb71806bd4b80bd390b9ed142d89e35b62e4af
NikitaYatsko <archon04@mail.ru>
2025-04-09 13:44:51 +0300

    task 2 is done

--- main.js
+++ main.js
@@ -1,15 +1,35 @@
 function celsiusToFahrenheit(celsius) {
-    const fahrenheit = (celsius * 9/5) + 32;
+    const fahrenheit = (celsius * 9 / 5) + 32;
     return fahrenheit;
 }
+
 console.log(celsiusToFahrenheit(0));
 console.log(celsiusToFahrenheit(25));
 
+function isPrime(n) {
+    if (n <= 1) return false;
+    if (n === 2) return true;
+    if (n % 2 === 0) return false;
 
-const myArray = [1,2,3,4];
+    const sqrt = Math.sqrt(n);
+    for (let i = 3; i <= sqrt; i += 2) {
+        if (n % i === 0) return false;
+    }
+    return true;
+}
+
+console.log(isPrime(7));
+console.log(isPrime(10));
+console.log(isPrime(2));
+
+
+const myArray = [1, 2, 3, 4];
+
 /*const myArray = ["a","b","c"];*/
 
-function reverseArray(arrayToReverse){
+function reverseArray(arrayToReverse) {
     return arrayToReverse.reverse();
 }
+
 console.log(reverseArray(myArray));
+
