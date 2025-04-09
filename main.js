f5da645140856c21553b8101afb3347ba80583dd
NikitaYatsko <archon04@mail.ru>
2025-04-09 14:03:57 +0300

    task 5 is done

--- main.js
+++ main.js
@@ -46,3 +46,12 @@ const list = document.getElementById('myList');
 appendButton.addEventListener('click', () => {
     list.innerHTML += `<li>New Item</li>`;
 });
+
+//task 5
+
+function objectToArray(obj) {
+    return Object.entries(obj)
+}
+
+console.log(objectToArray({name: 'Alice', age: 25}));
+console.log(objectToArray({city: 'New York', country: 'USA'}));
